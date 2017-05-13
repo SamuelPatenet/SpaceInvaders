037563919110a0ec157abcd92d0b820d785831db
Samuel Patenet <Samuel@PC>
2017-05-13 13:46:17 +0200

    refactoring du ToString

--- src/main/java/fr/unilim/iut/etu/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/etu/spaceinvaders/SpaceInvaders.java
@@ -16,11 +16,6 @@ public class SpaceInvaders {
 		this.hauteur = hauteur;
 	}
 
-	@Override
-	public String toString() {
-		return recupererEspaceJeuDansChaineASCII();
-	}
-
 	public String recupererEspaceJeuDansChaineASCII() {
 		StringBuilder espaceDeJeu = new StringBuilder();
 		for (int y = 0; y < hauteur; y++) {
@@ -61,5 +56,12 @@ public class SpaceInvaders {
 	private boolean estDansEspaceJeu(int x, int y) {
 		return ((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur));
 	}
-
+	
+	public void deplacerVaisseauVersLaDroite() {
+        if (vaisseau.abscisse()< (longueur-1)) vaisseau.seDeplacerVersLaDroite();
+	}
+	
+	public void deplacerVaisseauVersLaGauche() {
+        if (vaisseau.abscisse()> (1)) vaisseau.seDeplacerVersLaGauche();
+	}
 }
